c8a778897dc9ca88070553d46da77a28b428a46a
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2026-01-03 22:23:32 +0530

    Day 5 part 1 solution

---
+++ 2025/problem5/part1.py
@@ -0,0 +1,21 @@
+from utils import FileIO as iO
+
+def solve():
+    lines = iO.parse('input.txt', {'separator': 'nl'})
+
+    ranges = [list(map(int, line.split('-'))) for line in lines if '-' in line]
+    ingredients = [int(line) for line in lines if '-' not in line and len(line)]
+
+    fresh_ingredients = [get_fresh_ingredients(ingredient, ranges) for ingredient in ingredients]
+    print(sum(fresh_ingredients))
+
+
+def get_fresh_ingredients(ingredient, ranges) -> int:
+    for ingredient_range in ranges:
+        if ingredient in range(ingredient_range[0], ingredient_range[1] + 1):
+            return 1
+    return 0
+
+
+if __name__ == '__main__':
+    solve()
