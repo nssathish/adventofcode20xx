de406d7614aecf569df8bf6b26ec46a70b305a3b
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2026-01-06 14:00:02 +0530

    Day 5 part 2 solution

---
+++ 2025/problem5/part2.py
@@ -0,0 +1,24 @@
+from utils import FileIO as iO
+
+def solve():
+    lines = iO.parse('input.txt', {'separator': 'nl'})
+
+    ingredient_ids = sorted(list(map(int, line.split('-'))) for line in lines if '-' in line)
+
+    previous_end = 0
+    count = 0
+    for start, end in ingredient_ids:
+        if start > previous_end:
+            count += end - start + 1
+            previous_end = end
+        else:
+            overlap = end - previous_end
+            if overlap > 0:
+                count += overlap
+                previous_end = end
+
+    print(count)
+
+
+if __name__ == '__main__':
+    solve()
